175f161a3714e38b842d74fc36abe7bd69d3c429
Selvagopi2 <selvagopi2@gmail.com>
2024-01-05 11:31:08 +0530

    app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import LeftSidebar from './components/sidebar/sidebar';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import PageContent from './components/sidebar/pagecontent';
+import Login from '../src/components/login/Login'
+import PopularPacks from '../src/components/popularpacks/PopularPacks'
+import BundleDetails from '../src/components/bundledetails/BundleDetails'
+import Homepage from '../src/components/homepage/Homepage'
 
 function App() {
   return (
     <div className="drawer lg:drawer-open">
       <input id="left-sidebar-drawer" type="checkbox" className="drawer-toggle" />
       <Router>
-            <LeftSidebar/>
-            <PageContent/>
+            
+            <Routes>
+            <Route path='/login' element={<Login />}/>
+            <Route path="/popularpacks" element={<PopularPacks/>}/>
+            <Route path="/bundledetails" element={<BundleDetails/>}/>
+            <Route path='/homepage' element={<Homepage/>}/>
+            </Routes>
+
       </Router>
 
     
