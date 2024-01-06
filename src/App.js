68300deb7ac9368c66ab07380570217224a3b239
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2024-01-06 15:59:59 +0530

    fvgjghjk

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import Login from '../src/components/login/Login'
 import PopularPacks from '../src/components/popularpacks/PopularPacks'
 import BundleDetails from '../src/components/bundledetails/BundleDetails'
 import Homepage from '../src/components/homepage/Homepage'
+<<<<<<< Updated upstream
+=======
+import PageContent from './components/sidebar/pagecontent';
+import LeftSidebar from './components/sidebar/sidebar';
+import FrontPage from './components/frontpage/FrontPage';
+import Welcome from './components/welcome/Welcome';
+import StartPage from './components/startpage/StartPage';
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -18,7 +26,14 @@ function App() {
             <Route path='/login' element={<Login />}/>
             <Route path="/popularpacks" element={<PopularPacks/>}/>
             <Route path="/bundledetails" element={<BundleDetails/>}/>
+<<<<<<< Updated upstream
             <Route path='/homepage' element={<Homepage/>}/>
+=======
+            <Route path='/dashboard' element={<Homepage/>}/>
+            <Route path='/FrontPage' element={<FrontPage/>}/>
+            <Route path='/Welcome' element={<Welcome/>}/>
+            <Route path='/StartPage' element={<StartPage/>}/>
+>>>>>>> Stashed changes
             </Routes>
 
       </Router>
